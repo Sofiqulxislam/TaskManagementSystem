f76d769c68e82878e604189996794abfae68fa9f
Sofiqul Sohan <105537289+Sofiqulxislam@users.noreply.github.com>
2024-05-02 02:31:50 +0600

    V1.0

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <title>Workflow Management</title>
+    <!-- jQuery file -->
+    <script src="includes/juqery_latest.js"></script>
+    <!-- Bootstrap files -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!-- External CSS file -->
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+<body>
+    <div class="container">
+    <div class="row">
+        <div class="col-md-6 m-auto" id="home_page">
+            <center><h3>Task Management System</h3><hr>
+            <a href="user_login.php" class="btn btn-success" style="margin-right: 10px;">User Login</a>
+            <a href="admin/admin_login.php" class="btn btn-success"style="margin-right: 10px;">Admin Login</a>
+            <a href="register.php" class="btn btn-warning">User Registration</a></center>
+        </div>
+    </div>
+</div>
+    
+</body>
+</html>
